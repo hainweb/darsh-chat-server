4f6181422290d6bfedece1533da46d4c3a19d6d9
hainweb <hainofficialweb@gmail.com>
2024-07-19 13:46:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { app, server } = require('./socket/index')
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+//    allowedHeaders: ["Content-Type", "Authorization"],
     credentials : true
 }))
 app.use(express.json())

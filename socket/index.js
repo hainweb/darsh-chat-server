9f218786843eda9ecf46a2fd8bfb836f9013e74f
hainweb <hainofficialweb@gmail.com>
2024-07-19 18:11:55 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
   //  methods: ["GET", "POST"],
-  //  allowedHeaders: ["Content-Type", "Authorization"],
+   allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
    
   }
